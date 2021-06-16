955701ed98d30c909c92f84160931d9a3ddac104
IvoPenha <85446995+IvoPenha@users.noreply.github.com>
2021-06-16 10:23:06 -0300

    Update population.c
    Uma população de lhamas da vida a 1/3 de sua população em novas lhamas por meio de partos todo ano, enquanto isso, 1/4 de sua população falece todo ano. O algorítimo consiste no usuário digitar o numero inicial de lhamas e o numero esperado final, então o programa calcula e imprime quantos anos demorarão até chegar ao resultado. 
    
    A population of lhamas has 1/3 of it's own number birthing every year, cocunrentlly, 1/4 of the population dies every year. The algorithm consist in the user typing the number initial of lhamas and the expected number at the end, so the program tells how many years gonna take to the population reach that number

--- population.c
+++ population.c
@@ -1,66 +1,30 @@
-#include <stdio.h>
 #include <cs50.h>
-#include <string.h>
-#include <ctype.h>
-#include <stdlib.h>
+#include <stdio.h>
 
-bool isValid(string s);
-
-int main(int argc, string argv[])
+int birth();
+int death();
+int main(void)
 {
-    if ((argc != 2) || (isValid(argv[1]) == 0))
+    int years = 0;
+     int number;
+  do
+  {
+      number = get_int("Start size: ");
+  }
+  while (number < 9);
+   int end;
+  do
+  {
+      end= get_int("End size: ");
+  }
+  while (end < number);
+    while (number<end)
     {
-        printf("./caesar key\n");
-        return 1;
+        int birth = number/3;
+        int death = number/4;
+        number = number + birth -death ;
+        years++;
     }
-    else
-    {
-            
-        string alphacount = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";// a string that's contain the alphabetical digits
-            
-        string plaintext = get_string("plaintext: ");
-        int num = atoi(argv[1]);// a variable that  stores the number typed by the user
-        int len = strlen(plaintext);
-        for (int i = 0; i < len; i++)
-        {
-                
-            if (isupper(plaintext[i]))
-            {
-                plaintext[i] = (int) plaintext[i] % (int)'A';//modulum of 'A' so it stores a number from 0 to 25, for implement in alphacount
-                int temp = ((int) plaintext[i] + num) % 26 ;
-                plaintext[i] = alphacount[(temp)];
-            }
-            else if (islower(plaintext[i]))
-            {
-                plaintext[i] = plaintext[i] % 'a';//modulum of 'a' so it stores a number from 0 to 25, for implement in alphacount
-                int temp = ((int) plaintext[i] + num) % 26 ;
-                plaintext[i] = alphacount[(temp)];//alphacount only stores uppercase so it requires the tolower extension
-                plaintext[i] = tolower(plaintext[i]);//that is of lowercase letters so it must continue as lowercase
-                   
-            }
-            else
-            {
-                plaintext[i] = plaintext[i];
-            }
-        
-            printf("ciphertext: %s\n", plaintext);
-            
-        }
+    printf("Years: %i\n", years);
     
-    }
 }
-
-    bool isValid(string s)
-    {
-        int len = strlen(s);
-        for (int i = 0; i < len; i++)
-        {
-            if (isdigit(s[i]) == false)
-            {
-                return 0;
-                break;
-            }
-        }
-    
-        return 1;
-    }
